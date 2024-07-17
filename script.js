48d99bb0523afbda149360e3e41dcc2761d36598
Mylian Gedhe <54980322+MyukaQA@users.noreply.github.com>
2024-07-17 16:39:52 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,2 @@
+document.getElementById('bannerImage').src = 'your-banner-image-path.jpg'; // Replace with actual banner image path
+document.getElementById('profileImage').src = 'your-profile-image-path.jpg'; // Replace with actual profile image path
